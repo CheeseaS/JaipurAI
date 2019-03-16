c043f3410bee96a4fd16394ffe2daf0dac024c28
Chris M <chris@techfocus.net>
2019-03-16 00:41:21 -0700

    added stuff to the swap function

--- RewardAgent.py
+++ RewardAgent.py
@@ -3,15 +3,24 @@ import copy
 
 # reward based agent
 def GenerateMove(hand, market, tokens):
-
-
-    handscore = scoreHand(hand,tokens)
-    print(market)
     print(hand)
-    handScore = handscore
-    getScore = getCard(hand,market,tokens)
-    print(handscore)
-    print(getScore)
+    print(market)
+    #handscore = max(goodVal)
+    handScore = scoreHand(hand,tokens)
+    cardNo = 0
+    getScore = 0
+    swapscore = 0
+    for i in range(0,6):
+        cardNo += hand[i]
+    #print(cardNo)
+    # check valid moves
+    if (cardNo < 7):
+        getScore = getCard(hand,market,tokens)
+    swapCheck = True
+    if swapCheck == True:
+        swapscore = SwapCards(hand, market, tokens, cardNo)
+    #print(handScore)
+    #print(getScore)
     return "foo"
 
 # check score in hand
@@ -20,56 +29,70 @@ def scoreHand(hand, tokens):
     #print(hand)
     maxScore = 0
     # for each good in hand:
-    for i in range (0,6):
+    for i in range(5, -1, -1):
+        #print(i)
         sum = 0
         if hand[i] != 0:
             if (tokens[i]):
                 stSz = len(tokens[i]) - 1
                 # check current point value for good
                 for j in range(0,hand[i]):
-
                     sum += tokens[i][(stSz - j)]
-            #print (hand[i])
+            # if bonus value applies, add value
             if hand[i] == 3:
-                #print("+2")
                 sum += 2
             if hand[i] == 4:
-                #print("+5")
                 sum += 5
             if hand[i] == 5:
-                #print("+9")
                 sum += 9
-            #print(sum)
             if sum > maxScore:
                 maxScore = sum
     return maxScore
 
-def getCard(hand, market, tokens):
-    maxScore = (0,0)
-    for i in range(0,6):
-        if market[i] != 0:
-            temphand = copy.deepcopy(hand)
-            temphand[i] +=1
-            #print(temphand)
-            tempScore = scoreHand(temphand,tokens)
-            if tempScore > maxScore[0]:
-                maxScore = (tempScore,i)
-    return maxScore
-
-
-
-# if bonus value applies, add value
-# handscore = max(goodVal)
-
-# check valid moves
-# for valid moves generate handscores:
 # check pick up value
 # for each good in market
 # generate hands based on player hand plus 1 good in market place
 # store max score and associated move
+def getCard(hand, market, tokens):
+    maxScore = (0,0)
+    # for valid moves generate handscores:
+    for i in range(6, -1,-1):
+        if market[i] != 0:
+            temphand = copy.deepcopy(hand)
+            temphand[i] += 1
+            tempScore = scoreHand(temphand,tokens)
+            if tempScore > maxScore[0]:
+                maxScore = (tempScore, i)
+    return maxScore
+
 # check swap value
+def SwapCards(hand, market, tokens, cardNo):
+    maxScore = 0
+    # determine cards to swap
+    validSwaps = []
+    for i in range (0,6):
+        if market[i] > 1:
+            validSwaps.append(i)
+
+    for j in validSwaps:
+        temphand = copy.deepcopy(hand)
+        #print(temphand)
+        if (cardNo + market[j]) < 8:
+            temphand[j] = temphand[j] + market[j]
+            print(temphand)
+            tempScore = scoreHand(temphand, tokens)
+            print(tempScore)
+    #break swaps into cases, swap < camel and swap > camel
+    # hand > 7
+
+
+
+
+
+
+
 # if (market[i] >= 2)
-# determine cards to swap
+
 # camels + goods[!max]
 # if (swap > get)
 # do swap
