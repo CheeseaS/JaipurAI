ad45c88288cbae606fa5928ac94d3e90b1bc3171
Chris M <chris@techfocus.net>
2019-03-16 16:32:59 -0700

    added driver

---
+++ driver.py
@@ -0,0 +1,10 @@
+# if player is learning agent pass game to learning agent
+# if player is reward based agent pass hand, market, and tokens
+# init game
+# set players
+# loop until end game
+#   player1 turn
+#   check end
+#   player2 turn
+#   check end
+# pass score(s) to learning agent(s)
