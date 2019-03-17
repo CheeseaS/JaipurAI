2b11d25d849168356228197dc024c8354acf69b3
Chris M <chris@techfocus.net>
2019-03-16 20:13:35 -0700

    added swap

--- RewardAgent.py
+++ RewardAgent.py
@@ -3,24 +3,25 @@ import copy
 
 # reward based agent
 def GenerateMove(hand, market, tokens):
-    print(hand)
-    print(market)
+    #print(hand)
+    #print(market)
     #handscore = max(goodVal)
     handScore = scoreHand(hand,tokens)
-    cardNo = 0
+    handSz = 0
     getScore = 0
     swapscore = 0
     for i in range(0,6):
-        cardNo += hand[i]
+        handSz += hand[i]
     #print(cardNo)
     # check valid moves
-    if (cardNo < 7):
+    if handSz < 7:
         getScore = getCard(hand,market,tokens)
-    swapCheck = True
-    if swapCheck == True:
-        swapscore = SwapCards(hand, market, tokens, cardNo)
-    #print(handScore)
-    #print(getScore)
+
+    if handSz > 1:
+        swapScore = SwapCards(hand, market, tokens)
+    print (getScore)
+    print (swapScore)
+
     return "foo"
 
 # check score in hand
@@ -43,7 +44,7 @@ def scoreHand(hand, tokens):
                 sum += 2
             if hand[i] == 4:
                 sum += 5
-            if hand[i] == 5:
+            if hand[i] >= 5:
                 sum += 9
             if sum > maxScore:
                 maxScore = sum
@@ -66,39 +67,37 @@ def getCard(hand, market, tokens):
     return maxScore
 
 # check swap value
-def SwapCards(hand, market, tokens, cardNo):
+def SwapCards(hand, market, tokens):
     maxScore = 0
     bestSwap = -1
     GTS = False
     # determine cards to swap
-    validSwaps = []
+    posSwaps = []
+    secondCheck(bestSwap, hand)
+
     for i in range (0,6):
         if market[i] > 1:
-            validSwaps.append(i)
+            posSwaps.append(i)
 
-    for j in validSwaps:
+    for j in posSwaps:
         temphand = copy.deepcopy(hand)
-        #print(temphand)
-
         temphand[j] = temphand[j] + market[j]
-
         tempScore = scoreHand(temphand, tokens)
         if tempScore > maxScore:
-            if (cardNo + market[j]) > 7:
-                GTS = True
             maxScore = tempScore
             bestSwap = j
 
-    if bestSwap == -1:
-        return (0,0)
-    print(maxScore,bestSwap,GTS)
+    swap = [0,0,0,0,0,0,0]
+    swap[bestSwap] = market[bestSwap]
 
-    #all swaps can be done by camels/
+    balanceSwap(swap,temphand)
+    for i in range(0,7):
+        temphand[i] = hand[i] + swap[i]
+    #all swaps can be done by camels
     if not GTS and hand[6] > market[bestSwap]:
         qty = market[bestSwap]
-        #print(qty)
         temphand[6] -= qty
-    print(temphand)
+    return (maxScore,swap)
     #handsize check
     #find cards to swap
     #break swaps into cases, swap < camel and swap > camel
@@ -112,13 +111,84 @@ def SwapCards(hand, market, tokens, cardNo):
     #goods swap
         #find number of goods to swap for bestSwap
             #sum goods from 0-6 until
+def balanceSwap(swap, hand):
+    handSz = 0
+    getting = swap.index(max(swap))
+    for i in range(0,6):
+        handSz += hand[i]
+    if handSz < 7:
+        overLimit(swap, hand,handSz,getting)
+    else:
+        simpleSwap(swap, hand, getting)
+    sum = 0
+    for i in range(0,7):
+        sum += swap[i]
+    if sum != 0:
+        swap[getting] -= sum
 
+def overLimit(swap, hand, handSz, getting):
+    handDiff = handSz - 7
+    i = 0
+    while (handDiff > 0) and (i < 7):
+        #print(i, handDiff, getting)
+        if i == getting:
+            i += 1
+        else:
+            good = hand[i]
+            if good == 0:
+                i += 1
+                continue
+            if good < handDiff:
+                swap[i] = -good
+                handDiff -= good
+                i += 1
+                continue
+            if good >= handDiff:
+                swap[i] = -handDiff
+                handDiff = 0
+    return swap
 
 #need swap format
 
+def secondCheck(bestSwap, hand):
+    swapNo = 0
+    if bestSwap == -1:
+        return False
+    for i in range(0,7):
+        if i != bestSwap:
+            swapNo += hand[i]
+    if swapNo < 2:
+        return False
+    return True
+
+def simpleSwap(swap, hand, getting):
+    if (hand[6] > getting):
+        swap[6] = -swap[getting]
+    else:
+        swap[6] = -hand[6]
+        remaining = swap[getting] + swap[6]
+        i = 0
+        while (remaining > 0) and (i < 7):
+            if i == getting:
+                i += 1
+            else:
+                good = hand[i]
+                if good <= 0:
+                    i += 1
+
+                    continue
+                if good < remaining:
+                    swap[i] = -good
+                    remaining -= good
+                    i += 1
+
+                    continue
+                if good >= remaining:
+                    swap[i] = -remaining
+                    remaining = 0
 
 
-
+    #print (swap)
 # if (market[i] >= 2)
 
 # camels + goods[!max]
